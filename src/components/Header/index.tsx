50bfebde2e67596bb797f83869b5530bda6bdbd7
anhtoan260497 <anhtoan260497@gmail.com>
2022-10-01 00:08:00 +0700

    continue header right

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
-import React, { Fragment, useState } from "react";
+import { Fragment, useState } from "react";
 import "./styles.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 interface menuOptions {
     label: string;
@@ -57,6 +59,19 @@ const initialHeaderMenuLeft: menuOptions[] = [
             { label: 'Research Reports' }
         ]
     },
+    {
+        label: 'Products',
+        childOptions: [
+            { label: 'GeckoTerminal', isNew: true },
+            { label: 'CoinGecko Premium' },
+            { label: 'CoinGecko App' },
+            { label: 'Crypto Portfolio' },
+            { label: 'Crypto API' },
+            { label: 'Crypto Widget' },
+            { firstItem: true, label: 'GeckoCon 2022' },
+            { label: 'CoinGecko Store' },
+        ]
+    }
 ]
 
 const Header = ({ props }: any) => {
@@ -64,16 +79,18 @@ const Header = ({ props }: any) => {
 
 
     const renderMenuList = () => {
-        return headerMenuLeft.map(item => {
+        return headerMenuLeft.map((item, idx) => {
             return (
-                <div className="menu-left-options">
+                <div className="menu-left-options" key={idx}>
                     <a href="/#" className="label">{item.label}</a>
                     <div className="menu-child-options flex flex-col justify-center items-center">
-                        {item.childOptions.map(childItem => {
+                        {item.childOptions.map((childItem, childIdx) => {
                             return (
                                 childItem.firstItem ?
-                                    <><div className="slash"></div>
-                                        <a className="child-option py-1" href="dasdas">{childItem.label}</a></> : <a className="child-option py-1" href="dasdas">{childItem.label}</a>
+                                    <Fragment key={childIdx}><div className="slash"></div>
+                                        <a className="child-option py-1" href="dasdas">
+                                            {childItem.label} <span className="new-label">New</span>
+                                        </a></Fragment> : <a className="child-option py-1" href="dasdas" key={childIdx}>{childItem.label}</a>
                             )
                         })}
                     </div>
@@ -83,14 +100,27 @@ const Header = ({ props }: any) => {
     }
 
     return (
-        <div className="header mx-auto flex justify-between">
-            <div className="header-left flex justify-between items-center">
-                <img
-                    className="header-logo"
-                    src="https://static.coingecko.com/s/coingecko-logo-8903d34ce19ca4be1c81f0db30e924154750d208683fad7ae6f2ce06c76d0a56.png"
-                    alt="coin-gecko-logo"
-                />
-                {renderMenuList()}
+        <div className="header-container-fluid">
+            <div className="header-container mx-auto flex justify-between">
+                <div className="header-left flex justify-between items-center">
+                    <img
+                        className="header-logo"
+                        src="https://static.coingecko.com/s/coingecko-logo-8903d34ce19ca4be1c81f0db30e924154750d208683fad7ae6f2ce06c76d0a56.png"
+                        alt="coin-gecko-logo"
+                    />
+                    {renderMenuList()}
+                </div>
+                <div className="header-right flex justify-between items-center">
+                    <a className="anchor-option" href="##">Portfolio</a>
+                    <a className="anchor-option" href="##">Login</a>
+                    <a className="header-signup" href="##">Sign Up</a>
+                    <div className="header-right-input">
+                        <input type="text" placeholder="Search" />
+                        <FontAwesomeIcon className="icon" icon={faMagnifyingGlass} />
+                    </div>
+                    <div className="suggest-">
+                        </div>
+                </div>
             </div>
         </div>
     );
